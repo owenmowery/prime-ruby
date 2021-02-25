9dc055c786c2e00ff33f2a1ab1f5c6f3c5192c3a
Owen Mowery <owenmowery99@gmail.com>
2021-02-25 14:32:59 -0500

    commit tree f679816fd2359e339638a78f7e07f7a7c96473b7

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+    if int <= 1 || int == 0 || int == 1
+        return false
+    else
+        (2..int - 1).each do |i|
+            if int % i == 0
+                return false
+            end
+        end
+    end
+    true
+end
